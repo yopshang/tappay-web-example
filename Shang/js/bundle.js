ef6763223a7e7076216c7c64ca4c8b5858459486
Shang <yopshang@gmail.com>
2023-08-30 17:27:15 +0800

    feature: 打包功能

---
+++ Shang/js/bundle.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function NWP(){
+    function setForm(){
+        // const $NWP =  document.querySelector('.NWP')
+        var $iframe = document.createElement("iframe");
+        $iframe.src = "http://127.0.0.1:5500/fakeiframe.html";
+        $iframe.width = "400";
+        $iframe.height = "100";
+        $iframe.frameBorder ="0";
+        $iframe.scrolling = "0";
+        $iframe.style.border= "none";
+        $iframe.style.background = "white";
+        document.querySelector('.NWP').appendChild($iframe);
+    }
+    
+    // document.querySelector(".load_iframe").addEventListener('click', function(){
+        setForm();
+    // })
+})();
