db3e02f13db1196732dc6c142f902041f9c36d4b
Shang <yopshang@gmail.com>
2023-08-31 14:42:34 +0800

    feature: add more credit card varify rules

--- Shang/fakeiframe.js
+++ Shang/fakeiframe.js
@@ -1,9 +1,27 @@
 (function(){
     let CardNumber = document.getElementById('CardNumber')
     CardNumber.addEventListener('input', (e)=>{
-        identify_card_type(e.target.value)
+        varify_bank_handler(e.target.value)
     })
-    function identify_card_type(from_this_number){
+    function varify_bank_handler(credit_card_number){
+        let bank = identify_card_type(credit_card_number)
+        console.log('bank:', bank)
 
+    }
+    function identify_card_type(from_this_number){
+        let current_bank_code = Number(from_this_number[0]);
+        // Master Card: 5
+        // Visa : 4
+        // JCB: 3
+        switch(current_bank_code){
+            case 5:
+                return 'MasterCard';
+            case 4:
+                return 'Visa';
+            case 3:
+                return 'JCB';
+            default:
+                return null;
+        }
     }
 })()
