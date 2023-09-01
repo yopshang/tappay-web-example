989b38a158f7ad9b28c95ed249a3708bf214127e
Shang <yopshang@gmail.com>
2023-09-01 14:26:40 +0800

    feature: 信用卡號驗證

--- Shang/fakeiframe.js
+++ Shang/fakeiframe.js
@@ -4,8 +4,23 @@
             varify_bank_handler(e.target.value)
         })
         function varify_bank_handler(credit_card_number){
-            let bank = identify_card_type(credit_card_number)
-            checkIfNumberAvaliable(credit_card_number)
+            const bank = identify_card_type(credit_card_number)
+            
+            let evenTotal = evenNumTotal(credit_card_number)
+            let oddTotal = oddNumTotal(credit_card_number)
+            let sum = evenTotal + oddTotal
+            console.log('sum:', sum)
+            
+            let resultNum = sum%10;
+            if(resultNum != 0){
+                resultNum = 10 - resultNum
+            }
+            if(bank === null || resultNum != (credit_card_number[15]) * 1)  {
+                console.log('bank:', bank, 'resultNum:', resultNum, 'credit_card_number[15]) * 1', (credit_card_number[15]) * 1)
+                console.log('無效卡號')
+            } else {
+                console.log('通過！！！！！！')
+            }
     
         }
         function identify_card_type(from_this_number){
@@ -25,29 +40,25 @@
                     return null;
             }
         }
-        function checkIfNumberAvaliable(credit_card_number){
-            let evenTotal = evenNumTotal(credit_card_number)
-            let oddTotal = oddNumTotal(credit_card_number)
-            console.log('evenTotal:', evenTotal, 'oddTotal:', oddTotal);
-        }
         function evenNumTotal(cardNumber){
-            if(cardNumber.length < 14) return null
-
+            // if(cardNumber.length < 16) return null
             let sum = 0;
             for(let i =1; i<= 13; i +=2){
                 let number = Number(cardNumber[i]);
                 sum += number
             }
-            console.log('sum',sum)
+            console.log('even:', sum)
             return sum;
         }
         function oddNumTotal(cardNumber) {
-            if(cardNumber.length < 16) return null
+            // if(cardNumber.length < 16) return null
             let sum = 0;
             for (let i = 0; i <= 15; i += 2) {
-                let number = Number(cardNumber[i]);
+                let number = Number(cardNumber[i]) * 2;
+                if(number >= 10) number -=9
                 sum += number
             }
+            console.log('odd', sum)
             return sum;
           }
     class Error {
